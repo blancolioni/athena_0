fce7cc760ac2a36861ed8754593ceb213ee08610
Fraser Wilson <fraser@thewhitelion.eu>
2020-05-08 08:47:03 +0200

    Updated scripts

--- clean-setup.bat
+++ clean-setup.bat
@@ -1,4 +1,5 @@
 @build\bin\athena-driver --create
+@build\bin\athena-driver --add-empire --name=Liam --plural=Liams --color=#4363d8 --capital=Nijmegen
 @build\bin\athena-driver --add-empire --name=Musitello --color=#911eb4 --capital=Aramael
 @build\bin\athena-driver --add-empire --name=Atreides --color=#e6194b --capital=Caladan
 @build\bin\athena-driver --add-empire --name=Harkonnen --color=#3cb44b --capital="Geidi Prime"
@@ -8,7 +9,6 @@
 @build\bin\athena-driver --add-empire --name=Komnenos --plural=Komnenoi --color=#46f0f0 --capital=Kastamon
 @build\bin\athena-driver --add-empire --name=Hapsburg --plural=Hapsburgs --color=#f032e6 --capital=Vienna
 @build\bin\athena-driver --add-empire --name=Plantagenet --plural=Plantagenets --color=#bcf60c --capital=London
-@build\bin\athena-driver --add-empire --name=Liam --plural=Liams --color=#4363d8 --capital=Nijmegen
 @build\bin\athena-driver --add-empire --name=Rome --plural=Romans --adjective=Roman --color=#008080 --capital=Rome
 @build\bin\athena-driver --add-empire --name=Terra --plural=Terrans --adjective=Terran --color=#e6beff --capital=Sol
 @build\bin\athena-driver --add-empire --name=Theodora --plural=Theodorans --adjective=Theodoran --color=#9a6324 --capital=Cherson
