a9fda3c1ffa5a6173413caa590a88cd50b120a4e
Fraser Wilson <fraser@thewhitelion.eu>
2020-05-12 17:01:17 +0200

    Athena.Treaties: new procedure Declare_War

--- src/empires/athena-treaties.adb
+++ src/empires/athena-treaties.adb
@@ -1,3 +1,5 @@
+with Athena.Logging;
+
 with Athena.Handles.Relationship.Selections;
 
 package body Athena.Treaties is
@@ -15,6 +17,31 @@ package body Athena.Treaties is
    begin
       return Get_Relationship (E1, E2).War;
    end At_War;
+
+   -----------------
+   -- Declare_War --
+   -----------------
+
+   procedure Declare_War
+     (E1, E2 : Athena.Handles.Empire.Empire_Class)
+   is
+      R_To   : constant Athena.Handles.Relationship.Relationship_Class :=
+                 Get_Relationship (E1, E2);
+      R_From : constant Athena.Handles.Relationship.Relationship_Class :=
+                 Get_Relationship (E2, E1);
+      New_Opinion : constant Integer :=
+                      R_From.Opinion - 100;
+   begin
+      R_To.Update_Relationship
+        .Set_War (True)
+        .Done;
+      R_From.Update_Relationship
+        .Set_War (True)
+        .Set_Opinion (New_Opinion)
+        .Done;
+      Athena.Logging.Log
+        (E1.Name & " declared war on " & E2.Name);
+   end Declare_War;
 
    ----------------------
    -- Get_Relationship --
